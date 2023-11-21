b6cb81b442388d803703f129c099dc784dbe4517
Jones3bc <Jones3bc@cmich.edu>
2023-11-21 18:17:51 -0500

    Implemented labs 6, 7, and 8

--- labs/lab_06/test_scores.js
+++ labs/lab_06/test_scores.js
@@ -1,10 +1,102 @@
+/*
+ * @Author Brock Jones
+ * @Date 11/21/2023
+ * @Purpose To supply a program that can average test scores, find the highest test score,
+ * add test scores, and display all test scores.
+ */
 "use strict";
 const $ = selector => document.querySelector(selector);
 
 const names = ["Ben", "Joel", "Judy", "Anne"];
 const scores = [88, 98, 77, 88];
 
+/**
+ * Calculates the average and highest score and
+ * displays these values on the page.
+ */
+function displayResults(){
+	let scoreTotal = 0;
+	let highScore = scores[0];
+	let highScoreName = scores[0];
+
+	for(let scoreIndex in scores){
+		scoreTotal += scores[scoreIndex];
+
+		if(scores[scoreIndex] > highScore) {
+			highScore = scores[scoreIndex];
+			highScoreName = names[scoreIndex];
+		}
+	}
+
+	console.log(scoreTotal);
+	let div = $("#results");
+	div.innerHTML = ''; //Clears div elements.
+
+	let h2 = document.createElement("h2");
+	h2.textContent = "Results";
+
+	let p1 = document.createElement("p");
+	p1.textContent = "Average score = " + (scoreTotal / scores.length);
+
+	let p2 = document.createElement("p");
+	p2.textContent = "High score = " + highScoreName + " with a score of " + highScore;
+
+	div.appendChild(h2);
+	div.appendChild(p1);
+	div.appendChild(p2);
+}
+
+/**
+ * Displays all names and scores on the page.
+ */
+function displayScores (){
+	let div = $("#scores");
+	div.innerHTML = '';
+
+	let h2 = document.createElement("h2");
+	h2.textContent = "Scores";
+	div.appendChild(h2);
+
+	for(let scoreIndex in scores) {
+		let nameLabel = document.createElement("label");
+		nameLabel.textContent = names[scoreIndex];
+
+		let scoreLabel = document.createElement("label");
+		scoreLabel.textContent = scores[scoreIndex];
+
+		div.appendChild(nameLabel);
+		div.appendChild(scoreLabel);
+		div.appendChild(document.createElement("br"));
+	}
+}
+
+/**
+ * Adds a name and score to the names and scores arrays.
+ */
+function addScore(){
+	let newName = $("#name").value;
+	let newScore = $("#score").value;
+	console.log(newScore);
+
+	if(newName === "" || newScore === "" || isNaN(newScore) || newScore > 100 || newScore < 0){
+		$("#name").nextElementSibling.textContent = "Please enter a name.";
+		$("#score").nextElementSibling.textContent = "Score must be between 0 and 100.";
+		return;
+	}
+
+	names[names.length] = newName;
+	scores[scores.length] = Number(newScore);
+
+	//Reset values and refocus name input box.
+	$("#name").nextElementSibling.textContent = "";
+	$("#score").nextElementSibling.textContent = "";
+	$("#name").value = "";
+	$("#score").value = "";
+	$("#name").focus();
+}
+
 document.addEventListener("DOMContentLoaded", () => {
+	$("#name").focus();
 	// add event handlers
 	$("#add").addEventListener("click", addScore);
 	$("#display_results").addEventListener("click", displayResults);
--- labs/lab_07/clock.js
+++ labs/lab_07/clock.js
@@ -1,13 +1,42 @@
+/*
+ * @Author Brock Jones
+ * @Date 11/21/2023
+ * @Purpose To supply a program that shows the current time (EST) and has a stopwatch tool.
+ */
 "use strict";
 const $ = selector => document.querySelector(selector);
 
 const padSingleDigit = num => num.toString().padStart(2, "0");
 
 const displayCurrentTime = () => {
+    const currentTime = new Date().getTime();
+
+    let seconds = Math.floor(currentTime / 1000);
+    let minutes = Math.floor(seconds / 60);
+    seconds = seconds % 60;
+    let hours = Math.floor(minutes / 60) - 5; //-5 for Time convertion from UTC to EST (5 hour difference)
+    minutes = minutes % 60;
+    hours = hours % 24;
    
+    let am = true;
     // correct hours and AM/PM value for display
     if (hours > 12) { // convert from military time
-    } else { // adjust 12 noon and 12 midnight
+        hours = hours - 12;
+        am = false;
+    } 
+
+    if(hours == 0) {
+        $("#hours").textContent = "12"; //Set any 0 o'clocks to 12 o'clock.
+    } else {
+        $("#hours").textContent = hours.toString().padStart(2, '0');
+    }
+    $("#minutes").textContent = minutes.toString().padStart(2, '0');
+    $("#seconds").textContent = seconds.toString().padStart(2, '0');
+    
+    if(am) {
+        $("#ampm").textContent = "AM";
+    } else {
+        $("#ampm").textContent = "PM";
     }
 };
 
@@ -20,39 +49,60 @@ let elapsedMilliseconds = 0;
 
 const tickStopwatch = () => {
     // increment milliseconds by 10 milliseconds
-    
+    elapsedMilliseconds += 10;
     // if milliseconds total 1000, increment seconds by one and reset milliseconds to zero
-    
+    if(elapsedMilliseconds == 1000) {
+        elapsedSeconds++;
+        elapsedMilliseconds = 0;
+    }
     // if seconds total 60, increment minutes by one and reset seconds to zero
+    if(elapsedSeconds == 60) {
+        elapsedMinutes++;
+        elapsedSeconds = 0;
+    }
     
     //display new stopwatch time
-    
+    $("#s_minutes").textContent = elapsedMinutes.toString().padStart(2, '0');
+    $("#s_seconds").textContent = elapsedSeconds.toString().padStart(2, '0');
+    $("#s_ms").textContent = elapsedMilliseconds.toString().padStart(3, '0');
 };
 
 // event handler functions
 const startStopwatch = evt => {
     // prevent default action of link
+    evt.preventDefault();
         
     // do first tick of stop watch and then set interval timer to tick
     // stop watch every 10 milliseconds. Store timer object in stopwatchTimer
     // variable so next two functions can stop timer.
+    tickStopwatch();
+    stopwatchTimer = setInterval(tickStopwatch, 10);
     
 };
 
 const stopStopwatch = evt => {
     // prevent default action of link
-        
+    evt.preventDefault();
+
     // stop timer
-    
+    clearInterval(stopwatchTimer);
 };
 
 const resetStopwatch = evt => {
     // prevent default action of link
-        
+    evt.preventDefault();
+
     // stop timer
+    clearInterval(stopwatchTimer);
         
     // reset elapsed variables and clear stopwatch display
+    elapsedMinutes = 0;
+    elapsedSeconds = 0;
+    elapsedMilliseconds = 0;
     
+    $("#s_minutes").textContent = "00";
+    $("#s_seconds").textContent = "00";
+    $("#s_ms").textContent = "000";
 };
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -63,4 +113,7 @@ document.addEventListener("DOMContentLoaded", () => {
     setInterval(displayCurrentTime, 1000);
 	
 	// set up stopwatch event handlers
+    $("#start").onclick = startStopwatch;
+    $("#stop").addEventListener("click", stopStopwatch);
+    $("#reset").addEventListener("click", resetStopwatch);
 });
